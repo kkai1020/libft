41e4b9c45ce970d37af4d2c73fe126b4dc2c488d
kkai <kkai@student.42tokyo.jp>
2021-04-12 02:51:51 +0900

    first commit

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 02:20:32 by kkai              #+#    #+#             */
+/*   Updated: 2021/04/12 02:38:54 by kkai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	size_t	i;
+	size_t	j;
+	unsigned int	n;
+
+	i = 0;
+	j = 0;
+	n = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	if (size <= i)
+		return (size + j);
+	while (n < size - i - 1 && src[n] != '\0')
+	{
+		dest[i + n] = src[n];
+		n++;
+	}
+	dest[i + n] = '\0';
+	return (i + j);
+}
