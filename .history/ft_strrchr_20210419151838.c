0b6bb858f9d90b2008adc77b6d9ecd960b998faf
kkai <kkai@student.42tokyo.jp>
2021-04-19 15:33:46 +0900

    2

--- .history/ft_strrchr_20210419151838.c
+++ .history/ft_strrchr_20210419151838.c
@@ -6,7 +6,7 @@
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 10:50:14 by kkai              #+#    #+#             */
-/*   Updated: 2021/04/19 15:32:47 by kkai             ###   ########.fr       */
+/*   Updated: 2021/04/19 15:18:38 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ const char   *ft_strrchr(const char *str, int c)
 	size_t	i;
 
 	i = ft_strlen(str);
-    while (i >= 0)
+    while (str[i])
     {
         if (str[i] == c)
             return (&str[i]);
