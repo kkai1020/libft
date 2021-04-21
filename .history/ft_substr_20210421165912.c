baba0161ae32c50ff93767c44cc7d945079b1a5b
kkai <kkai@student.42tokyo.jp>
2021-04-21 17:38:08 +0900

    2

---
+++ .history/ft_substr_20210421165912.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/21 15:38:49 by kkai              #+#    #+#             */
+/*   Updated: 2021/04/21 16:59:12 by kkai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	n;
+
+	i = 0;
+	j = 0;
+	n = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		j++;
+	if (size <= i)
+		return (size + j);
+	while (n < size - i - 1 && src[n] != '\0')
+	{
+		dest[i + n] = src[n];
+		n++;
+	}
+	dest[i + n] = '\0';
+	return (i + j);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	unsigned char	*p;
+
+
+}
